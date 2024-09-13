2636c45495bb0d2edc23bb94557dca5fcef3a1bf
Cosaldi <cos.aldi007@gmail.com>
2024-09-13 21:14:07 +0700

    refactor: split module i3 and others

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,8 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+sleep 5
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
 polybar cosaldi 2>&1 | tee -a /tmp/polybar1.log & disown
